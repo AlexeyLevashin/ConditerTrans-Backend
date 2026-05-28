159413f1a20f32e76de11ff74ed5d5881304df63
Alexey <levalex814@gmail.com>
2026-05-28 23:31:58 +0300

    Fixed

--- Contracts/Auth/Requests/CreateAdminRequest.cs
+++ Contracts/Auth/Requests/CreateAdminRequest.cs
@@ -5,23 +5,6 @@ namespace Contracts.Auth.Requests;
 
 public class CreateAdminRequest
 {
-    [Required(ErrorMessage = "Email не может быть пустым")]
-    [EmailAddress(ErrorMessage = "Неверный формат email")]
-    public string Email { get; set; }
-    
-    [Required(ErrorMessage = "Пароль обязателен для заполнения")]
-    [MinLength(6, ErrorMessage = "Длина пароля должна быть не меньше 6 символов")]
-    public string Password { get; set; }
-    
-    [Required(ErrorMessage = "Телефон обязателен")]
-    [RegularExpression(@"^\+?[1-9]\d{10,14}$", ErrorMessage = "Некорректный формат телефона. Пример: +79991234567")]
-    public string Phone { get; set; }
-    
-    [Required(ErrorMessage = "Номер сотрудника обязателен")]
-    [StringLength(10, MinimumLength = 10, ErrorMessage = "Номер сотрудника должен состоять ровно из 10 символов")]
-    [RegularExpression(@"^\d{10}$", ErrorMessage = "Номер сотрудника должен содержать только цифры")]
-    public string EmployeeNumber { get; set; }
-    
     [Required(ErrorMessage = "Имя обязательно для заполнения.")]
     [MinLength(1, ErrorMessage = "Имя не может быть пустым.")]
     public string Name { get; set; }
@@ -32,4 +15,21 @@ public class CreateAdminRequest
     
     [MinLength(1, ErrorMessage = "Отчество не может быть пустым")]
     public string? Patronymic { get; set; }
+    
+    [Required(ErrorMessage = "Телефон обязателен")]
+    [RegularExpression(@"^\+?[1-9]\d{10,14}$", ErrorMessage = "Некорректный формат телефона. Пример: +79991234567")]
+    public string Phone { get; set; }
+    
+    [Required(ErrorMessage = "Номер сотрудника обязателен")]
+    [StringLength(10, MinimumLength = 10, ErrorMessage = "Номер сотрудника должен состоять ровно из 10 символов")]
+    [RegularExpression(@"^\d{10}$", ErrorMessage = "Номер сотрудника должен содержать только цифры")]
+    public string EmployeeNumber { get; set; }
+    
+    [Required(ErrorMessage = "Email не может быть пустым")]
+    [EmailAddress(ErrorMessage = "Неверный формат email")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "Пароль обязателен для заполнения")]
+    [MinLength(6, ErrorMessage = "Длина пароля должна быть не меньше 6 символов")]
+    public string Password { get; set; }
 }
