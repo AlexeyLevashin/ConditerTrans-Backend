42a17239ea2c7f8b45899b49d51aae55e9824dd0
artokit <kdsa3102@gmail.com>
2026-09-08 20:00:59 +0300

    chages

--- trans-bulki/src/hooks/useTripLocationSubscription.ts
+++
@@ -1,1 +0,0 @@
-export { useCargoLocationSubscription as useTripLocationSubscription } from './useCargoLocationSubscription';
--- trans-bulki/src/services/tripTrackingWebSocket.ts
+++
@@ -1,6 +0,0 @@
-export {
-  subscribeToCargoLocation as subscribeToTripLocation,
-  connectDriverTracking,
-  type CargoTrackingSubscription as TripTrackingSubscription,
-  type DriverTrackingPublisher,
-} from './cargoTrackingWebSocket';
